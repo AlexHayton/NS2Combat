31e9737f0ba507e1be62dbdca9acfb125fb42ee8
Marco Pattke <j1m_w3st@web.de>
2012-06-17 18:03:26 +0200

    implemented a short welcome message

--- lua/combat_NS2Gamerules.lua
+++ lua/combat_NS2Gamerules.lua
@@ -19,6 +19,9 @@ function CombatNS2Gamerules:OnLoad()
 
     ClassHooker:SetClassCreatedIn("NS2Gamerules", "lua/NS2Gamerules.lua")
     self:PostHookClassFunction("NS2Gamerules", "JoinTeam", "JoinTeam_Hook")
+    
+    ClassHooker:SetClassCreatedIn("Gamerules", "lua/Gamerules.lua")
+    self:PostHookClassFunction("Gamerules", "OnClientConnect", "OnClientConnect_Hook")
 	
 end
 
@@ -45,6 +48,16 @@ function CombatNS2Gamerules:JoinTeam_Hook(self, player, newTeamNumber, force)
             // Printing the avg xp to the Server Console for testing
             Print(Experience_GetAvgXp())
         end    
+    end
+
+end
+
+// If the client connects, send him the welcome Message
+function CombatNS2Gamerules:OnClientConnect_Hook(self, client)
+    local player = client:GetControllingPlayer()
+    
+    for i, message in ipairs(combatWelcomeMessage) do
+        player:SendDirectMessage(message)  
     end
 
 end
