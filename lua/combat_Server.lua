7b35c800c34c5e67bea93b286e18c2823d963dc7
Alex Hayton <alex.hayton@gmail.com>
2012-07-08 18:38:14 +0100

    Added GameOvermind support.

--- lua/combat_Server.lua
+++ lua/combat_Server.lua
@@ -11,6 +11,12 @@
 Script.Load("lua/PathUtil.lua")
 Script.Load("lua/fsfod_scripts.lua")
 
+Script.Load("..\\NS2GmOvrmind\\Lua\\NS2GmOvrmind.lua"); -- Load the NS2-GmOvrmind prerequisites
+
+NS2GmOvrmind.InitFunc,CallRes=package.loadlib(string.format("%s\\Binaries_x86\\%s.dll",
+NS2GmOvrmind.Name.Internal,NS2GmOvrmind.Name.Internal),string.format("%s_Initialize",NS2GmOvrmind.Name.Internal));
+NS2GmOvrmind.InitFunc(1); -- Execute the initialization-function (in slave-mode)
+
 // Loading the Hook classes
 // TODO: Maybe we don't need the OnLoad?
 Script.Load("lua/combat_TechTreeHooks.lua")
@@ -59,6 +65,8 @@ Script.Load("lua/combat_Values.lua")
 // due to a bug, this needs to be loaded here
 Script.Load("lua/combat_PointGiverMixin.lua")
 Script.Load("lua/combat_ScoringMixin.lua")
+
+NS2GmOvrmind.InitFunc(2); -- Execute the initialization-function (in slave-mode)
 
 // Tell the class hooker that we've fully loaded.
 ClassHooker:OnLuaFullyLoaded()
