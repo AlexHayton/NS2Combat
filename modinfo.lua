4ba8b91e1a2b8f86a7d44b04b62c3a2d3f53e37b
AlexHayton <alex.hayton@gmail.com>
2012-07-17 13:24:13 +0100

    Add support for insertion into the mod list in fsfod's menu mod.

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+//________________________________
+//
+//   	NS2 Combat Mod     
+//	Made by JimWest and MCMLXXXIV, 2012
+//  This file kindly contributed by fsfod!
+//________________________________
+
+EngineBuild = 214
+ValidVM = {
+  client = true,
+  main = false,
+}
+
+ModTableName = "NS2CombatMenuMod"
+MainScript = "lua/combat_MenuModLauncher.lua"
+
+ScriptList = {
+  "lua/Hud/Marine/combat_GUIMarineHud.lua",
+  "lua/combat_Player_ClientHook.lua",
+}
+
