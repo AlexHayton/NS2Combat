7d8c9a112bfa85f63e05538cb9009f2db6709b9f
Alex Hayton <alex.hayton@gmail.com>
2012-11-08 08:12:10 +0000

    Fix the message when an upgrade is hardcapped

--- lua/combat_Player.lua
+++ lua/combat_Player.lua
@@ -515,8 +515,9 @@ if Server then
         elseif hint == "mutuallyExclusive" then
             self:SendDirectMessage( "Cannot buy this upgrade when you have the " .. type .. " upgrade!")
 			
-		elseif hint == "hardCap" then
-			self:SendDirectMessage( "Cannot buy this upgrade. Only 1 player may take this upgrade every 5 players in your team." )
+		elseif hint == "hardCapped" then
+			self:SendDirectMessage( "Cannot buy this upgrade.")
+			self:SendDirectMessage( "Only 1 player may take this upgrade for every 5 players in your team." )
             
         elseif hint == "freeLvl" then
             local lvlFree = self:GetLvlFree()
