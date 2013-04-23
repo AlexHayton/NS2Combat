9d55d4689afe117b23e003bf1aeb827a67ace851
Alex Hayton <alex.hayton@gmail.com>
2013-04-23 21:36:17 +0100

    Fixed stomp.

---
+++ lua/combat_StompMixin.lua
@@ -0,0 +1,11 @@
+//________________________________
+//
+//   	NS2 Combat Mod     
+//	Made by JimWest and MCMLXXXIV, 2012
+//
+//________________________________
+
+// changed it from tierthree to tiertwo
+function StompMixin:GetHasSecondary(player)
+    return player:GetHasTwoHives()
+end
