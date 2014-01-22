971fcc0b2e313d879ce7d529953c88f86b468f64
BrightPaul <sebi4d@gmx.de>
2014-01-22 11:48:18 +0100

    Removed all not needed uwe files

--- output/lua/combat_ScoringMixin.lua
+++ output/lua/combat_ScoringMixin.lua
@@ -6,12 +6,7 @@
 //________________________________
 
 // combat_ScoringMixin.lua
-
-
 // that we don't have to change everything, just do nothing, the cool score effect is now: Player:XpEffect()
 
-function ScoringMixin:AddScore(points, res)
-
-
-    
+function ScoringMixin:AddScore(points, res)   
 end
