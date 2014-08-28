6e327fc99fb835a5d96d791c91e11d294effdca3
Matt <mkreider@gmail.com>
2014-08-27 23:53:00 -0400

    Removal of movement mods no longer functional

--- output/lua/ExtraEntitiesMod/eem_MovementModifier.lua
+++ output/lua/ExtraEntitiesMod/eem_MovementModifier.lua
@@ -7,33 +7,6 @@
 
 Script.Load("lua/Class.lua")
 
-// Overrides the function so its not printing "system" all the time
-function Skulk:ConstrainMoveVelocity(moveVelocity)
-
-	// allow acceleration in air for skulks   
-	if not self:GetIsOnSurface() then
-	
-		local speedFraction = Clamp(self:GetVelocity():GetLengthXZ() / self:GetMaxSpeed(), 0, 1)
-		speedFraction = 1 - (speedFraction * speedFraction)
-		moveVelocity:Scale(speedFraction * Skulk.kAirAccelerationFraction)
-		
-	end
-	
-end
-
-// overrides onclampspeed so the force is the same to every class
-local originalPlayerOnClampSpeed
-originalPlayerOnClampSpeed = Class_ReplaceMethod( "Player", "OnClampSpeed", 
-	function(self, input, velocity)
-
-        // when not getting pushed, call the original method
-        if self.pushTime ~= -1 or self:isa("Lerk") then
-            originalPlayerOnClampSpeed(self, input, velocity)
-        end
-    end
-)
-
-
 if Server then
 
     // overrides OnJumpLand so the ClampSpeed can work right
